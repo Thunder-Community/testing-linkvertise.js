1ad0fe5c48192db3de156ed228909b3e497897c1
Thunder-Community <58745407+Thunder-Community@users.noreply.github.com>
2020-03-07 19:12:34 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch')
 
-module.exports = ({ key, url, whitelist }) =>
+module.exports = ({ id, url, whitelist }) =>
   fetch(`https://linkvertise.net/cdn/linkvertise.js/(${encodeURIComponent(url)}, {whitelist: [`${encodeURIComponent(whitelist)}`], blacklist: []})
   .then(res =>
     res.text()
